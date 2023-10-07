7c793c98c3004378066e984d2a00355ba87709fc
Art Tsai <arttsai@gmail.com>
2023-10-07 16:42:22 +0800

    Snake's constructor finished.

---
+++ ConsoleSnake/Point.cs
@@ -0,0 +1,12 @@
+﻿public class Point
+{
+    public int Y { get; set; }
+    public int X { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+}
--- ConsoleSnake/Snake.cs
+++ ConsoleSnake/Snake.cs
@@ -1,7 +1,46 @@
-﻿public class Snake
+﻿using ConsoleSnake;
+
+public class Snake
 {
+    private const int MinSnakeLength = 4;
+
+    private readonly Board _board;
+    private readonly LinkedList<Point> _body = new();
+    private readonly Point _foodPlace;
+    private readonly LinkedList<Direction> _keyList = new();
+
     public Snake(Board board)
     {
-        throw new NotImplementedException();
+        _board = board;
+
+        for (int i = 0; i < MinSnakeLength; i++) 
+            _body.AddLast(new Point(_board.Width / 2 + i, _board.Height / 2));
+        
+        // draw the snake on console
+        foreach (var point in _body)
+        {
+            Console.SetCursorPosition(point.X, point.Y);
+            Console.Write("@");
+        }
+
+        _foodPlace = PutFoodRandomly();
+
+        _keyList.AddLast(Direction.Left);
+
+    }
+
+    private Point PutFoodRandomly()
+    {
+        Point foodPoint;
+        do
+        {
+            foodPoint = new Point(
+                new Random().Next(0, _board.Width), 
+                new Random().Next(0, _board.Height));
+        } while (_body.Contains(foodPoint));
+        // draw the food on console
+        Console.SetCursorPosition(foodPoint.X, foodPoint.Y);
+        Console.Write("X");
+        return foodPoint;
     }
 }
